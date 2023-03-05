d0f3d4680b8575dd7bcc94755c1210c8039b0107
221RDB270 <125795302+221RDB270@users.noreply.github.com>
2023-03-05 21:27:13 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -5,23 +5,53 @@ import threading
 import numpy
 
 
-def compute_height(n, parents):
+def compute_height(n, parent):
     # Write this function
     max_height = 0
+    place = [root]
+    tree[[] for i in range(n) ]
+        for i in range(n): 
+            if parent[i] == -1:
+                root = i
+            else:
+                tree[parent[i]].append(i)
+        
+        while place:
+            next = []
+            for node in place:
+                next += tree[node]
+            place = next
+            max_height += 1
+             
+            
+            
     # Your code here
     return max_height
 
 
 def main():
     # implement input form keyboard and from files
-    
+    file = input()
+    if file[0] == "I":
+        file = input()
+    if file and 'a' not in file:
+        try:
+            with open(file, 'r') as file_1:
+                b = int(file_1.readline())
+                parent = list(map(int,flie_1.readline().split()))
+    else:
+        b = int(input())
+        parent = list(map(int, input().split()))
+        if b[0] == "I":
+            b = input()
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
-    
+    max_height = compute_height(n, parent)
+    print(max_height)
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
-    pass
+    
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
@@ -29,5 +59,5 @@ def main():
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
-main()
+
 # print(numpy.array([1,2,3]))
